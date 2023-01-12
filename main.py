4497a3e148f81c03a2f19f672ca3b6764f381dde
futoruma <evgeny.martov@gmail.com>
2023-01-12 15:06:31 +0900

    Changed the initial data.

--- main.py
+++ main.py
@@ -3,39 +3,39 @@ from openpyxl.utils import get_column_letter
 from openpyxl.styles import Font
 
 data = {
-    "Student 1": {
-        "math": 75,
-        "science": 78,
-        "history": 90
+    "Employee 1": {
+        "apples": 34000,
+        "melons": 58700,
+        "oranges": 13800
     },
-    "Student 2": {
-        "math": 79,
-        "science": 71,
-        "history": 72
+    "Employee 2": {
+        "apples": 28000,
+        "melons": 78200,
+        "oranges": 31030
     },
-    "Student 3": {
-        "math": 89,
-        "science": 87,
-        "history": 72
+    "Employee 3": {
+        "apples": 45000,
+        "melons": 11300,
+        "oranges": 91520
     }
 }
 
 wb = Workbook()
 ws = wb.active
-ws.title = "Grades"
+ws.title = "Stats"
 
-headings = ["Name"] + list(data["Student 1"].keys())
+headings = ["Name"] + list(data["Employee 1"].keys())
 ws.append(headings)
 
-for student in data:
-    grades = list(data[student].values())
-    ws.append([student] + grades)
+for employee in data:
+    stats = list(data[employee].values())
+    ws.append([employee] + stats)
 
-for col in range(2, len(data["Student 1"]) + 2):
+for col in range(2, len(data["Employee 1"]) + 2):
     char = get_column_letter(col)
     ws[char + "7"] = f"=SUM({char + '2'}:{char + '6'})/{len(data)}"
 
-for col in range(1, len(data["Student 1"]) + 2):
+for col in range(1, len(data["Employee 1"]) + 2):
     ws[get_column_letter(col) + "1"].font = Font(bold=True, color="0099CCFF")
 
-wb.save("NewGrades.xlsx")
+wb.save("CalculatedStats.xlsx")
